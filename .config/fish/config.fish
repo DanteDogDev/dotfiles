19d9712db4e98110850281f0435cbd4b5b030631
Dante Harper <dantedigdog@gmail.com>
2025-03-29 01:00:16 +0100

    added configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+function fish_greeting
+end
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias diff='delta'
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+alias dario='fastfetch'
