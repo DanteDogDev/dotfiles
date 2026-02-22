58b62fade13fa23da07c44197da79c2585ba00b7
Dante Harper <dantedigdog@gmail.com>
2026-02-22 18:35:27 +0100

    interesting new tab behaviour experimental

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -7,7 +7,8 @@ vim.filetype.add({
 })
 
 -- prettier,
-vim.lsp.enable({ "lua_ls" }) -- lua-language-server, style_lua,
+vim.lsp.enable({ "emmylua_ls" }) -- lua_ls alternative
+-- vim.lsp.enable({ "lua_ls" }) -- lua-language-server, style_lua,
 vim.lsp.enable({ "clangd" }) -- clangd,
-vim.lsp.enable({ "ruff","pyright" }) -- lsp for python
+vim.lsp.enable({ "ruff", "pyright" }) -- lsp for python
 -- vim.lsp.enable({ "rust_analyzer"}) -- rust-analyzer, -- INSTALL WITH PACKAGE MANAGER NOT MASON
