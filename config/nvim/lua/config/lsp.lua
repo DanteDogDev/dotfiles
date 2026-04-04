8b410f08c6a45ce9c4b6a2a1698362ead5fdeb27
Dante Harper <dantedigdog@gmail.com>
2026-04-04 17:16:47 +0200

    format

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -1,14 +1,14 @@
 -- https://github.com/neovim/nvim-lspconfig/tree/master/lsp
 
 vim.filetype.add({
-  extension = {
-    dox = "doxygen",
-  },
+	extension = {
+		dox = "doxygen",
+	},
 })
 
 -- prettier,
-vim.lsp.enable({ "emmylua_ls" }) -- lua
-vim.lsp.enable({ "ts_ls","html","cssls" }) -- javascript, html, css
-vim.lsp.enable({ "clangd" }) -- cpp
-vim.lsp.enable({ "ruff", "pyright" }) -- python
-vim.lsp.enable({ "nixd" }) -- nix
+vim.lsp.enable({ "emmylua_ls" })           -- lua
+vim.lsp.enable({ "ts_ls", "html", "cssls" }) -- javascript, html, css
+vim.lsp.enable({ "clangd" })               -- cpp
+vim.lsp.enable({ "ruff", "pyright" })      -- python
+vim.lsp.enable({ "nixd" })                 -- nix
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  -- { "wnkz/monoglow.nvim" },
-  -- { "projekt0n/github-nvim-theme" },
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme tokyonight")
-    end,
-  },
+	-- { "wnkz/monoglow.nvim" },
+	-- { "projekt0n/github-nvim-theme" },
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme tokyonight")
+		end,
+	},
 }
